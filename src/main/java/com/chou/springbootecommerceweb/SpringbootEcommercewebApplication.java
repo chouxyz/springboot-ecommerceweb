026320f7a9a852f685987fcee2085395e5ba4762
chou <maychoutw9@gmail.com>
2023-05-17 19:08:23 +0800

    init Spring Boot proj

---
+++ src/main/java/com/chou/springbootecommerceweb/SpringbootEcommercewebApplication.java
@@ -0,0 +1,13 @@
+package com.chou.springbootecommerceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommercewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommercewebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chou/springbootecommerceweb/SpringbootEcommercewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chou.springbootecommerceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommercewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
