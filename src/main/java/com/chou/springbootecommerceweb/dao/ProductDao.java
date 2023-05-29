e5a6354e4d7c49f0d167f7438da121fe0aae3680
chou <maychoutw9@gmail.com>
2023-05-29 13:02:14 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/chou/springbootecommerceweb/dao/ProductDao.java
+++ src/main/java/com/chou/springbootecommerceweb/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.chou.springbootecommerceweb.dao;
 import com.chou.springbootecommerceweb.dto.ProductRequest;
 import com.chou.springbootecommerceweb.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/chou/springbootecommerceweb/service/ProductService.java
+++ src/main/java/com/chou/springbootecommerceweb/service/ProductService.java
@@ -3,7 +3,11 @@ package com.chou.springbootecommerceweb.service;
 import com.chou.springbootecommerceweb.dto.ProductRequest;
 import com.chou.springbootecommerceweb.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
