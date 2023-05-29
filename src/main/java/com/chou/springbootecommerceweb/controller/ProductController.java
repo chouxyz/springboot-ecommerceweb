4ad5ec8a1521bbe4aeaa1363ef3f76732b0bb06f
chou <maychoutw9@gmail.com>
2023-05-29 18:10:53 +0800

    商品功能 - 實作查詢商品列表（查詢條件）

--- src/main/java/com/chou/springbootecommerceweb/controller/ProductController.java
+++ src/main/java/com/chou/springbootecommerceweb/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.chou.springbootecommerceweb.controller;
 
+import com.chou.springbootecommerceweb.constant.ProductCategory;
 import com.chou.springbootecommerceweb.dto.ProductRequest;
 import com.chou.springbootecommerceweb.model.Product;
 import com.chou.springbootecommerceweb.service.ProductService;
@@ -18,8 +19,11 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> productList = productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(
+            @RequestParam(required = false) ProductCategory category,
+            @RequestParam(required = false) String search
+    ) {
+        List<Product> productList = productService.getProducts(category, search);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- src/main/java/com/chou/springbootecommerceweb/service/ProductService.java
+++ src/main/java/com/chou/springbootecommerceweb/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.chou.springbootecommerceweb.service;
 
+import com.chou.springbootecommerceweb.constant.ProductCategory;
 import com.chou.springbootecommerceweb.dto.ProductRequest;
 import com.chou.springbootecommerceweb.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 
