3f74c3d160dc5981aa2d1ab5f787f9525a7ab9d9
chou <maychoutw9@gmail.com>
2023-06-09 15:33:20 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/chou/springbootecommerceweb/dao/impl/UserDaoImpl.java
+++ src/main/java/com/chou/springbootecommerceweb/dao/impl/UserDaoImpl.java
@@ -11,10 +11,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class UserDaoImpl  implements UserDao {
@@ -29,6 +26,23 @@ public class UserDaoImpl  implements UserDao {
 
         Map<String, Object> map = new HashMap<>();
         map.put("userId", userId);
+
+        List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
+
+        if(userList.size() > 0) {
+            return userList.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        String sql = "SELECT user_id, email, password, created_date, last_modified_date " +
+                "FROM user WHERE email = :email";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
 
         List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
 
